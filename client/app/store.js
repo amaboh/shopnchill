8336529603dcc072ff9ccdc25923ff3ce307040e
amaboh <amabocanon@gmail.com>
2022-08-16 12:07:04 +0100

    middleware extention

--- client/app/store.js
+++ client/app/store.js
@@ -2,11 +2,16 @@ const configStore = require('@reduxjs/toolkit').configureStore
 const cakeReducer = require('../features/Cake/cakeSlice')
 const coffeeReducer = require('../features/coffee/coffeSlice')
 
+const reduxLogger = require('redux-logger')
+
+const logger = reduxLogger.createLogger()
+
 const store = configStore({
     reducer: {
         cake: cakeReducer,
         coffee: coffeeReducer
-    }
+    },
+    middleware: (getDefaultMiddleware)=> getDefaultMiddleware().concat(logger)
 })
 
 module.exports = store
--- client/index.js
+++ client/index.js
@@ -2,11 +2,12 @@ const store = require('./app/store')
 const cakeActions = require('./features/Cake/cakeSlice').cakeActions
 const coffeeActions = require('./features/coffee/coffeSlice').coffeeActions
 
+// middleware
+
+
 console.log('Initial state', store.getState())
 
-const unsubscribe = store.subscribe(()=>{
-    console.log('Updated state', store.getState())
-})
+const unsubscribe = store.subscribe(()=> {})
 
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
